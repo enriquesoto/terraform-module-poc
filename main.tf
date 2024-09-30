fe110fc4c43548cae975e2f43b7c60516db7961d
Enrique Arturo Soto Mendoza <enriquesoto@bcp.com.pe>
2024-09-30 09:16:46 -0500

    test kubelogin

--- main.tf
+++ main.tf
@@ -20,9 +20,39 @@ provider "azurerm" {
   
 }
 
-data "azurerm_kubernetes_cluster" "kubernetes_cluster_pulled" {
-  name                = "aksveu2polyd01"
-  resource_group_name = "RSGREU2POLYD01"
+resource "azurerm_resource_group" "rgnolocal_update" {
+  name     = data.azurerm_resource_group.rgnolocal.name
+  location = data.azurerm_resource_group.rgnolocal.location
+
+  tags = {
+    Environment = "Dev"
+    Department  = "Banca"
+    Owner       = "Enriqueto"
+  }
+}
+
+
+resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
+  name                = "aksveu2ctpld99"
+  location            = azurerm_resource_group.rgnolocal_update.location
+  resource_group_name = azurerm_resource_group.rgnolocal_update.name
+  dns_prefix          = "aksveu2ctpld99"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    #   vm_size    = "Standard_D2_v2"
+    vm_size = "Standard_A2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+  tags = {
+    Environment = "Dev"
+    Department  = "Banca"
+    Owner       = "Enriqueto"
+  }
 }
 
 
