1b078fc546f05f4fcaedc7570061e39665eae71b
Enrique Arturo Soto Mendoza <enriquesoto@bcp.com.pe>
2024-10-02 09:31:36 -0500

    install gitops

--- main.tf
+++ main.tf
@@ -74,35 +74,35 @@ resource "azurerm_resource_provider_registration" "kubernetes_configuration" {
 
 
 
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.host
-  # client_certificate     = base64decode(data.azurerm_kubernetes_cluster.kubernetes_cluster_pulled.kube_config.0.client_certificate)
-  # client_key             = base64decode(data.azurerm_kubernetes_cluster.kubernetes_cluster_pulled.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.cluster_ca_certificate)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "./kubelogin"
-    args = [
-      "get-token",
-      "--environment",
-      "AzurePublicCloud",
-      "--server-id",
-      "6dae42f8-4368-4678-94ff-3960e28e3630",
-      "--client-id",
-      var.client_id,
-      "--client-secret",
-      var.client_secret,
-      "--tenant-id",
-      var.tenant_id,
-      "--login",
-      "spn",
-      "|",
-      "jq",
-      ".status.token"
-    ]
-  }
+# provider "kubernetes" {
+#   host                   = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.host
+#   # client_certificate     = base64decode(data.azurerm_kubernetes_cluster.kubernetes_cluster_pulled.kube_config.0.client_certificate)
+#   # client_key             = base64decode(data.azurerm_kubernetes_cluster.kubernetes_cluster_pulled.kube_config.0.client_key)
+#   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.cluster_ca_certificate)
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     command     = "./kubelogin"
+#     args = [
+#       "get-token",
+#       "--environment",
+#       "AzurePublicCloud",
+#       "--server-id",
+#       "6dae42f8-4368-4678-94ff-3960e28e3630",
+#       "--client-id",
+#       var.client_id,
+#       "--client-secret",
+#       var.client_secret,
+#       "--tenant-id",
+#       var.tenant_id,
+#       "--login",
+#       "spn",
+#       "|",
+#       "jq",
+#       ".status.token"
+#     ]
+#   }
 
-}
+# }
 
 # resource "kubernetes_cluster_role" "developer_env_desa_aks" {
 #   metadata {
